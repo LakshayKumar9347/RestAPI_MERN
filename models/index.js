b5bb5826a7ab9be0367f5c62fc1d0e820793477f
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2023-05-05 06:15:02 +0530

    fix minor bug on mongoose schema
    fix data adding/pushing to database

--- models/index.js
+++ models/index.js
@@ -1,33 +1,33 @@
 const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require:true
+    name: {
+        type: String,
+        required: true,
     },
-    price:{
-        type:Number,
-        require:[true,"Price must be provided"]
+    price: {
+        type: Number,
+        required: [true, "Price must be provided"],
     },
-    featured:{
-        type:Boolean,
-        require:false
+    featured: {
+        type: Boolean,
+        default: false,
     },
-    rating:{
-        type:Number,
-        require:4.9
+    rating: {
+        type: Number,
+        default: 4.9,
     },
-    createdAt:{
-        type:Date,
-        default:Date.now()
+    createdAt: {
+        type: Date,
+        default: Date.now()
     },
-    company:{
-        type:String,
-        enum:{
-            values:["apple","samsung","dell","mi"],
-            message:`{values} is not supported`
+    company: {
+        type: String,
+        enum: {
+            values: ["apple", "samsung", "dell", "nokia"],
+            message: `{VALUE} is not supported`
         }
     },
 })
 
 
-module.exports = mongoose.model('product',productSchema)
+module.exports = mongoose.model('Product', productSchema)
